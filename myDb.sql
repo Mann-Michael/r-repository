b143cc9e52db8c3eb9192c653fe80133b64839e4
Mann-Michael <uno.mann@gmail.com>
2018-12-16 13:40:13 -0600

    Add files via upload

--- myDb.sql
+++ myDb.sql
@@ -1,16 +1,5 @@
 -- table seeding
 
-CREATE TABLE s_user
-( s_user_id SERIAL PRIMARY KEY
-, s_user_name VARCHAR(255) NOT NULL
-, s_user_password VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE keyword
-( keyword_id SERIAL PRIMARY KEY
-, keyword_name VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE measurement
 ( measurement_id SERIAL PRIMARY KEY
 , measurement_name VARCHAR(255) NOT NULL
@@ -20,20 +9,14 @@ CREATE TABLE recipe
 ( recipe_id SERIAL PRIMARY KEY
 , recipe_name VARCHAR(255) NOT NULL
 , recipe_desc VARCHAR(255)
-, s_user_id INT REFERENCES s_user 
-);
-
-CREATE TABLE recipe_keyword_group
-( recipe_id INT REFERENCES recipe
-, keyword_id INT REFERENCES keyword
 );
 
 CREATE TABLE ingredient
 ( ingredient_id SERIAL PRIMARY KEY
 , ingredient_name VARCHAR(255) NOT NULL
-, amount DECIMAL NOT NULL
+, amount FLOAT8 NOT NULL
 , measurement_id INT NOT NULL
-, recipe_id INT REFERENCES recipe
+, recipe_id INT REFERENCES recipe NOT NULL
 );
 
 CREATE TABLE instruction
@@ -49,78 +32,7 @@ CREATE USER dbuser WITH PASSWORD '1234';
 GRANT SELECT, INSERT, UPDATE, DELETE ON s_user, keyword, measurement, recipe, recipe_keyword_group, ingredient, instruction TO dbuser;
 GRANT USAGE, SELECT ON SEQUENCE s_user_s_user_id_seq, keyword_keyword_id_seq, measurement_measurement_id_seq, recipe_recipe_id_seq, ingredient_ingredient_id_seq, instruction_instruction_id_seq TO dbuser;
 
--- s_user seeding
-
-INSERT INTO s_user
-( s_user_name
-, s_user_password
-)
-VALUES
-( 'Michael'
-, '1234'
-);
-
-INSERT INTO s_user
-( s_user_name
-, s_user_password
-)
-VALUES
-( 'Devyn'
-, '1234'
-);
-
-INSERT INTO s_user
-( s_user_name
-, s_user_password
-)
-VALUES
-( 'Iris'
-, '1234'
-);
-
--- keyword seeding
-
-INSERT INTO keyword
-( keyword_name
-)
-VALUES
-( 'Indian'
-);
-
-INSERT INTO keyword
-( keyword_name
-)
-VALUES
-( 'French'
-);
-
-INSERT INTO keyword
-( keyword_name
-)
-VALUES
-( 'American'
-);
-
-INSERT INTO keyword
-( keyword_name
-)
-VALUES
-( 'Italian'
-);
-
-INSERT INTO keyword
-( keyword_name
-)
-VALUES
-( 'Chinese'
-);
-
-INSERT INTO keyword
-( keyword_name
-)
-VALUES
-( 'Japanese'
-);
+ALTER TABLE ingredient ALTER COLUMN amount TYPE float8;
 
 -- measurement seeding
 
@@ -177,89 +89,34 @@ VALUES
 
 INSERT INTO recipe 
 ( recipe_name
-, recipe_desc
-, s_user_id)
+, recipe_desc)
 VALUES
 ( 'Cheerios'
 , 'This is a plain bowl of cereal'
-, 1
 );
 
 INSERT INTO recipe 
 ( recipe_name
-, recipe_desc
-, s_user_id)
+, recipe_desc)
 VALUES
 ( 'Honey Cheerios'
 , 'This is a sweet bowl of cereal'
-, 3
 );
 
 INSERT INTO recipe 
 ( recipe_name
-, recipe_desc
-, s_user_id)
+, recipe_desc)
 VALUES
 ( 'Smoothie'
 , 'This is an amazing smoothie'
-, 2
 );
 
 INSERT INTO recipe 
 ( recipe_name
-, recipe_desc
-, s_user_id)
+, recipe_desc)
 VALUES
 ( 'Cheese and Crackers'
 , 'This is almost all I know how to make'
-, 1
-);
-
--- recipe_keyword_group seeding
-
-INSERT INTO recipe_keyword_group
-( recipe_id
-, keyword_id
-)
-VALUES
-( 1
-, 3
-);
-
-INSERT INTO recipe_keyword_group
-( recipe_id
-, keyword_id
-)
-VALUES
-( 2
-, 3
-);
-
-INSERT INTO recipe_keyword_group
-( recipe_id
-, keyword_id
-)
-VALUES
-( 2
-, 2
-);
-
-INSERT INTO recipe_keyword_group
-( recipe_id
-, keyword_id
-)
-VALUES
-( 3
-, 1
-);
-
-INSERT INTO recipe_keyword_group
-( recipe_id
-, keyword_id
-)
-VALUES
-( 4
-, 4
 );
 
 -- ingredient seeding
